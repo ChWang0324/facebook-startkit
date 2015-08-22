9fdbca6c3ceb8f9eba0c32d889747747a346a8dc
abcdefgs0324 <abcdefgs0324@gmail.com>
2015-08-22 17:10:13 +0800

    Update key.js

--- js/key.js
+++ js/key.js
@@ -1,10 +1,11 @@
 /*請貼上fackbook初始化的相關程式碼*/
 /*https://developers.facebook.com/*/
- window.fbAsyncInit = function() {
+ <script>
+  window.fbAsyncInit = function() {
     FB.init({
-      appId      : '838463579536966',
+      appId      : '372316362939046',
       xfbml      : true,
-      version    : 'v2.3'
+      version    : 'v2.4'
     });
   };
 
@@ -15,6 +16,7 @@
      js.src = "//connect.facebook.net/en_US/sdk.js";
      fjs.parentNode.insertBefore(js, fjs);
    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 
 
